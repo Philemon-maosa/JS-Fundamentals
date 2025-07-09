0e131e8e4be4392d66539e0b4decf3c91c618eae
philemon <philemonmaosa@gmail.com>
2025-07-09 16:47:51 +0300

    An integer

---
+++ 5-to_integer.js
@@ -0,0 +1,9 @@
+const arg = process.argv[2];
+
+const number = Number(arg);
+
+if (!isNaN(number) && Number.isInteger(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log("Not a number");
+}
