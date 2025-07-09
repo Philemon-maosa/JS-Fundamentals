00749efb015f1557186c226769cf0970650c550e
philemon <philemonmaosa@gmail.com>
2025-07-09 14:20:01 +0300

    I have rectified an error on the code

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -2,4 +2,6 @@ const firstline = "C";
 const secondline = "Python is cool";
 const thirdline = "Javascript is amazing";
 
-console.log("firstline\nsecondline\nthirdline");
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
