34644691fb0c022f540a58c4589e643b8adaa8af
philemon <philemonmaosa@gmail.com>
2025-07-09 16:32:18 +0300

    Edited the code

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,7 +1,7 @@
-const firstline = "C is fun";
-const secondline = "Python is cool";
-const thirdline = "Javascript is amazing";
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "Javascript is amazing";
 
-console.log(firstline);
-console.log(secondline);
-console.log(thirdline);
+console.log(line1);
+console.log(line2);
+console.log(line3);
